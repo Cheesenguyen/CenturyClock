0fe78753e1420acd19aef799739e1db3112a18e5
Cheesenguyen <nguyenthuychi0206@gmail.com>
2025-09-08 17:36:45 +0700

    create all header and .c file

---
+++ inc/constant.h
@@ -0,0 +1,51 @@
+#define CYCLE_1S_WAIT      3        //Số lần đếm ngược còn lại trước khi báo hiệu 1 giây đã trôi qua
+
+#define DIRECTION_INCREASE true
+#define DIRECTION_DECREASE false
+
+#define MAX_VALUE_SECOND   59
+#define MAX_VALUE_MINUTE   59
+#define MAX_VALUE_HOUR     23
+
+#define MAX_VALUE_DAY_28   28
+#define MAX_VALUE_DAY_29   29
+#define MAX_VALUE_DAY_30   30
+#define MAX_VALUE_DAY_31   31
+
+#define MAX_VALUE_MONTH    12
+#define MAX_VALUE_YEAR     9999
+
+// Default time values
+#define DEFAULT_SECOND       0      //editable
+#define DEFAULT_MINUTE       30     //editable
+#define DEFAULT_HOUR         7      //editable
+#define DEFAULT_DAY          11     //editable
+#define DEFAULT_MONTH        5      //editable
+#define DEFAULT_YEAR         2024   //editable
+
+// Colors
+#define COLOR_DEFAULT        0xFFFF00   //editable
+#define COLOR_BACKGROUND     0x0        //editable
+
+// Font size
+#define FONT_WIDTH           5
+#define FONT_HEIGHT          8
+
+// Number width
+#define NUMBER_WIDTH_2       2
+#define NUMBER_WIDTH_4       4
+
+// Position values
+#define POS_SS_X             1
+#define POS_SS_Y             1
+#define POS_MI_X             16
+#define POS_MI_Y             1
+#define POS_HH_X             31
+#define POS_HH_Y             1
+#define POS_DD_X             1
+#define POS_DD_Y             13
+#define POS_MO_X             16
+#define POS_MO_Y             13
+#define POS_YY_X             31
+#define POS_YY_Y             13
+
