ecba855cf373ecea493784c8e8705b8cc55fab05
Cheesenguyen <nguyenthuychi0206@gmail.com>
2025-09-12 14:27:08 +0700

    chay duoc, output theo chieu ngang

--- inc/ledmatrix.h
+++ inc/ledmatrix.h
@@ -9,7 +9,7 @@
 #include "ecall.h"
 
 void LEDMATRIX_DisplayDigit(int digit, int x, int y, uint32_t color);
-void LEDMATRIX_DisplayRow(const uint8_t *row, int width, int x, int y, uint32_t color);
+void LEDMATRIX_DisplayRow(const uint8_t row, int width, int x, int y, uint32_t color);
 void LEDMATRIX_DisplayPixel(int x, int y, uint32_t color);
 
 #endif
--- src/ledmatrix.c
+++ src/ledmatrix.c
@@ -13,21 +13,22 @@ void LEDMATRIX_DisplayPixel(int x, int y, uint32_t color);
 
 void LEDMATRIX_DisplayDigit(int digit, int x, int y, uint32_t color){
     int height = FONT_HEIGHT;
-    const uint8_t * const *p_row = g_p_font_digit + digit;
+    const uint8_t *p_row = g_p_font_digit + digit;
 
     while(height > 0){
         height = height - 1;
         p_row = p_row + FONT_WIDTH;
         LEDMATRIX_DisplayRow(*p_row, FONT_WIDTH, x, y, color);
+
     }
 }
 
-void LEDMATRIX_DisplayRow(const uint8_t *row, int width, int x, int y, uint32_t color){
+void LEDMATRIX_DisplayRow(const uint8_t row, int width, int x, int y, uint32_t color){
     while (1)
     {
         int width = width - 1;
         if(width >= 0){
-            if((*row) & (0x1 << width) == 1){
+            if((row) & (0x1 << width) == 1){
                 LEDMATRIX_DisplayPixel(x, y, color);
             }
             else{
